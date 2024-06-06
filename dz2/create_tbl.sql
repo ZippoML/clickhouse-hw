0f9afd84952076c0b18f3123db9bc9a89d6532c7
Student <apmakhutov@gmail.com>
2024-06-06 22:18:09 +0300

    dz 2 started

---
+++ dz2/create_tbl.sql
@@ -0,0 +1,22 @@
+CREATE database if not exists testdb;
+ 
+USE testdb;
+
+drop table if exists winemag;
+
+create table winemag (
+ 	id UInt32 PRIMARY KEY,
+    country String,
+    description String,
+    title String,
+    points UInt8,
+    price Float64,
+    province String,
+    region_1 String,
+    region_2 String,
+    taster_name String,
+    taster_twitter_handle String,
+    designation String,
+    winery String,
+    variety String
+) Engine = MergeTree();
